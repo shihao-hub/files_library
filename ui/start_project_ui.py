91f1a77e5528b000468be730f7eddea87baa0e03
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-06-24 14:31:19 +0800

    Update start_project_ui.py

--- ui/start_project_ui.py
+++ ui/start_project_ui.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import sys
+import time
 import traceback
 from datetime import datetime
 import pprint
@@ -31,71 +33,127 @@ class MainWindow(Ui_MainWindow):
 
         main_window.setWindowFlags(main_window.windowFlags() & ~Qt.WindowMaximizeButtonHint)
 
-        self.pushButton.clicked.connect(lambda: self.text_collation())
-        self.pushButton.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.pushButton.customContextMenuRequested.connect(self.set_menu_for_pushButton)
+        self.text_collation_button.clicked.connect(self.text_collation)
+        self.text_collation_button.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.text_collation_button.customContextMenuRequested.connect(self.set_menu_for_text_collation_button)
 
-        self.pushButton_3.clicked.connect(lambda: self.text_remove_duplicates())
-        self.pushButton_3.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.pushButton_3.customContextMenuRequested.connect(self.set_menu_for_pushButton_3)
+        self.text_rm_dup_button.clicked.connect(self.text_remove_duplicates)
+        self.text_rm_dup_button.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.text_rm_dup_button.customContextMenuRequested.connect(self.set_menu_for_text_rm_dup_button)
 
-        self.current_time_timer = QTimer()
-        self.current_time_timer.timeout.connect(lambda: self.update_current_time(main_window))
-        self.current_time_timer.start(1000)  # 设置定时器每秒更新一次
-        self.label_2.setStyleSheet("color: black;")
-        # self.label_2.setStyleSheet("background-color: white; color: black;")
-        # self.label_2.setFixedHeight(40)
-        self.label_2.setWordWrap(True)
-        # self.label_2.setFont(QFont("微软雅黑", 10))
-        # self.label_2.setVisible(True)
-        self._set_current_time_for_label_2()
+        self.timer_for_current_time_label = QTimer()
+        self.timer_for_current_time_label.timeout.connect(self.update_current_time)
+        self.timer_for_current_time_label.start(1000)  # 设置定时器每秒更新一次
+        self.current_time_label.setStyleSheet("color: black;")
+        # self.current_time_label.setStyleSheet("background-color: white; color: black;")
+        # self.current_time_label.setFixedHeight(40)
+        self.current_time_label.setWordWrap(True)
+        # self.current_time_label.setFont(QFont("微软雅黑", 10))
+        # self.current_time_label.setVisible(True)
+        self._set_current_time_for_current_time_label()
 
-        self.set_menu_actions_callback()
+        self.md5_button.clicked.connect(self.callback_for_md5)
 
-    def callback_for_actionopen(self):
+        self.input_clear_button.clicked.connect(lambda: self.input_text_edit.clear())
+        self.input_clear_button.hide()
+        self.output_clear_button.clicked.connect(lambda: self.output_text_edit.clear())
+        self.output_clear_button.hide()
+        self.log_clear_button.clicked.connect(lambda: self.log_text_edit.clear())
+
+        self.actionTest.triggered.connect(self.callback_for_actionTest)
+
+        self.actionOpen.setShortcut("Ctrl+H")
+        self.actionOpen.triggered.connect(self.callback_for_actionOpen)
+
+    def callback_for_md5(self):
+        pass
+
+    def callback_for_actionTest(self):
+        def assert_equal(first, second, msg=None):
+            if first != second:
+                if isinstance(first, str):
+                    first = first.replace("\r", r"\r")
+                    first = first.replace("\n", r"\n")
+                    first = first.replace("\t", r"\t")
+                    second = second.replace("\r", r"\r")
+                    second = second.replace("\n", r"\n")
+                    second = second.replace("\t", r"\t")
+                raise AssertionError(f'{first} != {second}{"" if not msg else " : " + msg}')
+
+        write = sys.stderr.write
+        time_s = time.time()
+        count, max_count = 0, 1
+        try:
+            # 文本整理
+            self.input_text_edit.setPlainText("我is李华")
+            self.text_collation()
+            output = self.output_text_edit.toPlainText()
+            assert_equal(output, "我 is 李华", "text_collation error")
+            count += 1  # 需要手动自增，这个不好，需要优化
+            self.input_text_edit.clear()
+            self.output_text_edit.clear()
+
+            # 文本去重
+            self.input_text_edit.setPlainText("abc\r\nabc\r\ndef")
+            self.text_remove_duplicates()
+            output = self.output_text_edit.toPlainText()
+            assert_equal(output, "abc\ndef", "text_remove_duplicates error")
+            count += 1
+            self.input_text_edit.clear()
+            self.output_text_edit.clear()
+        except:
+            write(traceback.format_exc())
+            write("\n")
+
+            write(f"Ran {count + 1} test in {(time.time() - time_s):.4f}s\n\n")
+            write(f"FAILED (failures={max_count - count})\n\n")
+        else:
+            count = count  # 占个位置
+
+            write(f"Ran {count} test in {(time.time() - time_s):.4f}s\n\n")
+            write("OK\n\n")
+        finally:
+            pass
+
+    def callback_for_actionOpen(self):
         filename, _ = QFileDialog.getSaveFileName(self.main_window,
                                                   'Save File',
                                                   '',
                                                   'Text Files (*.txt);;All Files (*)')
 
-    def set_menu_actions_callback(self):
-        # Test
-        self.actionopen.setShortcut("Ctrl+H")
-        self.actionopen.triggered.connect(self.callback_for_actionopen)
-
     def logger(self, msg: str, level=0):
         prefix = (" INFO", "DEBUG", "FATAL")[level]
-        self.plainTextEdit_3.appendPlainText("["
-                                             + datetime.now().strftime(f"{'%Y-%m-%d %H:%M:%S'[0 if not DEBUG else 9:]}")
-                                             + " "
-                                             + prefix
-                                             + "]:  "
-                                             + msg)
+        self.log_text_edit.appendPlainText("["
+                                           + datetime.now().strftime(f"{'%Y-%m-%d %H:%M:%S'[0 if not DEBUG else 9:]}")
+                                           + " "
+                                           + prefix
+                                           + "]:  "
+                                           + msg)
 
-    def set_menu_for_pushButton(self, position):
-        menu = QMenu(self.pushButton)
+    def set_menu_for_text_collation_button(self, position):
+        menu = QMenu(self.text_collation_button)
 
         action1 = QAction("仅处理剪切板")
         action1.triggered.connect(lambda: self.text_collation(True))
         menu.addAction(action1)
 
-        menu.exec_(self.pushButton.mapToGlobal(position))
+        menu.exec_(self.text_collation_button.mapToGlobal(position))
 
-    def set_menu_for_pushButton_3(self, position):
-        menu = QMenu(self.pushButton_3)
+    def set_menu_for_text_rm_dup_button(self, position):
+        menu = QMenu(self.text_rm_dup_button)
 
         action1 = QAction("仅处理剪切板")
         action1.triggered.connect(lambda: self.text_remove_duplicates(True))
         menu.addAction(action1)
 
-        menu.exec_(self.pushButton_3.mapToGlobal(position))
+        menu.exec_(self.text_rm_dup_button.mapToGlobal(position))
 
-    def update_current_time(self, main_window):
-        self._set_current_time_for_label_2()
+    def update_current_time(self):
+        self._set_current_time_for_current_time_label()
 
-    def _set_current_time_for_label_2(self):
+    def _set_current_time_for_current_time_label(self):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.label_2.setText(current_time)
+        self.current_time_label.setText(current_time)
 
     def text_collation(self, enable_clip=None):
 
@@ -110,9 +168,9 @@ class MainWindow(Ui_MainWindow):
                 pyperclip.copy(pangu.spacing_text(text))
                 del text
             else:
-                text = self.plainTextEdit.toPlainText()
+                text = self.input_text_edit.toPlainText()
                 res_len = len(text)
-                self.plainTextEdit_2.setPlainText(pangu.spacing_text(text))
+                self.output_text_edit.setPlainText(pangu.spacing_text(text))
                 del text
         except:
             print(traceback.format_exc())
@@ -145,11 +203,11 @@ class MainWindow(Ui_MainWindow):
                 res = []
                 text = pyperclip.paste()
                 res_len = len(text)
-                lines = re.compile(r"\r*\n").split(self.plainTextEdit.toPlainText())
+                lines = re.compile(r"\r*\n").split(self.input_text_edit.toPlainText())
                 for line in self._dedup_for_text_remove_duplicates(lines):
                     res.append(line)
                 # res_len = len(res) - 1 + sum(len(e) for e in res)
-                self.plainTextEdit_2.setPlainText("\n".join(res))
+                self.output_text_edit.setPlainText("\n".join(res))
                 del res, lines, text
         except:
             print(traceback.format_exc())
@@ -187,7 +245,11 @@ class Ui_MainWindow_Test(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def test_for_all(self):
+        pass
+
     def test_for_pdf_merger(self):
+        self.assertEqual("1", "2")
         import PyPDF2
 
         in_files = [
