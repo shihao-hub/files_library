bf47af5571cf831746f0299b77df4e1c3f4388e1
shihao-hub <2958017271@qq.com>
2024-06-27 01:03:59 +0800

    main:xiaoluban

--- projects/get_dts_script.py
+++ projects/get_dts_script.py
@@ -1,7 +1,9 @@
 import re
+import time
 from datetime import datetime
 import os
 import zipfile
+from tqdm import tqdm
 
 base_file_path = r"D:\games\Steam\steamapps\common\Don't Starve Together\data\databundles"
 file_path = os.path.join(base_file_path, "scripts.zip")
@@ -9,6 +11,16 @@ file_path = os.path.join(base_file_path, "scripts.zip")
 base_target_path = r"E:\ProgrammingProjects\IDEAProjects"
 target_path = os.path.join(base_target_path, "zzz_scripts_" + datetime.now().strftime("%Y%m%d"))
 
+time_s = time.time()
 with zipfile.ZipFile(file_path) as zif_obj:
-    zif_obj.extractall(path=target_path)
+    members = None
+    if members is None:
+        members = zif_obj.namelist()
 
+    for zipinfo in tqdm(members):
+        zif_obj.extract(zipinfo, path=target_path, pwd=None)
+
+    # zif_obj.extractall(path=target_path)
+    print(f"解压成功，"
+          f"压缩包共 {sum(info.file_size for info in zif_obj.infolist()) / 1024 / 1024:.2f} MB，"
+          f"耗时 {time.time() - time_s:.2f} s")
---
+++ projects/xiaoluban.py
@@ -0,0 +1,51 @@
+import pprint
+import urllib.parse
+
+import requests
+
+from requests.status_codes import codes as status
+
+
+class ISCLDistributor:
+    def __init__(self, base_url: str, api1: str = None, api2: str = None):
+        self.base_url = base_url.endswith("/") and base_url or base_url + "/"
+        self.api1 = api1
+        self.api2 = api2
+
+    @classmethod
+    def _process_api_format(cls, api):
+        return api.startswith("/") and api.replace("/", "", 1) or api
+
+    @classmethod
+    def _check_status_ok(cls, response):
+        if response.status_code != status.ok:
+            raise requests.exceptions.HTTPError(f"response.status_code{response.status_code} != {status.ok}")
+
+    def get_one_record(self, query=None, api=None):
+        api = api or self.api1
+        url = self.base_url + self._process_api_format(api)
+        if query:
+            url += "?" + urllib.parse.urlencode(query)
+        response = requests.get(url, verify=False)
+        self._check_status_ok(response)
+        return response.json()
+
+    def distribute(self, data, api=None):
+        api = api or self.api2
+        url = self.base_url + self._process_api_format(api)
+        response = requests.post(url, json=data, verify=False)
+        self._check_status_ok(response)
+        return response.json()
+
+
+class PreValidation(ISCLDistributor):
+    pass
+
+
+pre_validation = PreValidation("https://httpbin.org/", "get", "post")
+pprint.pprint(pre_validation.get_one_record(query={
+    "name": "zsh"
+}))
+pprint.pprint(pre_validation.distribute(data={
+    "employee_id": "213191443"
+}))
