fb9688eb325bec31ba7229db3d939029451d33ac
shihao-hub <2958017271@qq.com>
2024-06-27 01:12:22 +0800

    general commit

--- projects/xiaoluban.py
+++ projects/xiaoluban.py
@@ -9,11 +9,13 @@ from requests.status_codes import codes as status
 class ISCLDistributor:
     def __init__(self, base_url: str, api1: str = None, api2: str = None):
         self.base_url = base_url.endswith("/") and base_url or base_url + "/"
-        self.api1 = api1
-        self.api2 = api2
+        self.api1 = self._process_api_format(api1)
+        self.api2 = self._process_api_format(api2)
 
     @classmethod
     def _process_api_format(cls, api):
+        if not api:
+            return api
         return api.startswith("/") and api.replace("/", "", 1) or api
 
     @classmethod
@@ -22,8 +24,8 @@ class ISCLDistributor:
             raise requests.exceptions.HTTPError(f"response.status_code{response.status_code} != {status.ok}")
 
     def get_one_record(self, query=None, api=None):
-        api = api or self.api1
-        url = self.base_url + self._process_api_format(api)
+        api = self._process_api_format(api) or self.api1
+        url = self.base_url + api
         if query:
             url += "?" + urllib.parse.urlencode(query)
         response = requests.get(url, verify=False)
@@ -31,8 +33,8 @@ class ISCLDistributor:
         return response.json()
 
     def distribute(self, data, api=None):
-        api = api or self.api2
-        url = self.base_url + self._process_api_format(api)
+        api = self._process_api_format(api) or self.api2
+        url = self.base_url + api
         response = requests.post(url, json=data, verify=False)
         self._check_status_ok(response)
         return response.json()
