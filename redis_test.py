b2b7126166bf18ce80569f44a76db7bfa7f48a24
shihao-hub <76770370+shihao-hub@users.noreply.github.com>
2024-08-09 02:53:36 +0800

    Update redis_test.py

--- redis_test.py
+++ redis_test.py
@@ -1,4 +1,7 @@
 """
+requirement.txt
+    redis==3.
+
 Redis
     conn.sadd
         self.execute_command('SADD', name, *values)
@@ -22,104 +25,127 @@ PairsNamedTuple = collections.namedtuple("PairsNamedTuple", [
     "article_pair_key",
 ])
 
-pairs = PairsNamedTuple(*[
-    "score:",
-    "time:",
-    "voted:",
-    "article:",
-])
 
-# const
-ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60
-VOTE_SCORE = 432
-ARTICLES_PER_PAGE = 25
+class Chapter01:
+    # 多余...
+    # PAIRS = PairsNamedTuple(*[
+    #     "score:",
+    #     "time:",
+    #     "voted:",
+    #     "article:",
+    # ])
+
+    # const
+    ONE_WEEK_IN_SECONDS = 7 * 24 * 60 * 60
+    VOTE_SCORE = 432
+    ARTICLES_PER_PAGE = 25
+
+    def __init__(self, connect: Redis):
+        self.connect = connect
+
+    @classmethod
+    def _get_article_id(cls, article_key: str) -> str:
+        return article_key.partition(":")[-1]
+
+    def _check_cutoff(self, article):
+        cutoff = time.time() - self.ONE_WEEK_IN_SECONDS
+        # 规定一篇文章发布期满一周之后，用户将不能再对它进行投票
+        if self.connect.zscore("score:", article) < cutoff:
+            return True
+        return False
+
+    def article_vote(self, user: str, article: str, reverse=False):
+        if self._check_cutoff(article):
+            return
+
+        article_id = self._get_article_id(article)
+        # Redis: 向数据库中添加 <voted:{article_id}, {user}> 键值对
+        if self.connect.sadd(f"voted:{article_id}", user):
+            # Redis: 增加 键为 score: 的 zset 类型中的 {article} 元素的分数值
+            self.connect.zincrby("score:",
+                                 self.VOTE_SCORE if not reverse else -self.VOTE_SCORE,
+                                 article)
+            # Redis: 增加 键为 {article} 的 hash 类型中的以 votes 为键的键值对的值
+            self.connect.hincrby(article, "votes", 1 if not reverse else -1)
+
+    def article_negative_vote(self, user: str, article: str):
+        """ 实现投反对票 """
+        return self.article_vote(user, article, reverse=True)
+
+    def post_article(self, user: str, title: str, link: str) -> str:
+        # Redis: 如果以 article: 为键的键值对不存在，则创建并初始化为 1。如果存在则加一。
+        #   conn.incr 在 python 中的返回值是 int 类型
+        article_id = str(self.connect.incr("article:"))
+
+        voted_key = f"voted:{article_id}"
+        self.connect.sadd(voted_key, user)
+        # 一周后删除投票用户的记录，因为一周后会冻结投票，显然不必留存了
+        self.connect.expire(voted_key, self.ONE_WEEK_IN_SECONDS)
+
+        now_time_stamp = time.time()
+        article_key = f"article:{article_id}"
+        # Redis: 此处是 python 封装的接口，设置 <{article_key}, hash> 键值对
+        self.connect.hmset(article_key, {
+            "title": title,
+            "link": link,
+            "poster": user,
+            "time": now_time_stamp,
+            "votes": 1  # 默认票数为 1
+        })
+
+        self.connect.zadd("score:", {article_key: now_time_stamp + self.VOTE_SCORE})
+        self.connect.zadd("time:", {article_key: now_time_stamp})
+
+        return article_id
+
+    def get_articles(self, page, order="score:"):
+        """ 分页返回功能 """
+        start = (page - 1) * self.ARTICLES_PER_PAGE
+        end = start + self.ARTICLES_PER_PAGE - 1
+
+        # Redis: 以 {order} 为键的键值对的值（zset 类型）的反向切片 [start, end]
+        ids = self.connect.zrevrange(order, start, end)
+        articles = []
+        for article_id in ids:
+            article_data = self.connect.hgetall(article_id)
+            # 添加字段
+            article_data["id"] = article_id
+            articles.append(article_data)
+        return articles
+
+    def add_remove_groups(self, article_id, to_add=(), to_remove=()):
+        """ 将文章添加到它所在的群组 或 从群组里面移除文章 """
+        article_key = f"article:{article_id}"
+        for e in to_add:
+            self.connect.sadd(f"group:{e}", article_key)
+        for e in to_remove:
+            self.connect.srem(f"group:{e}", article_key)
+
+    def get_group_articles(self, group, page, order="score:"):
+        key = order + group
+        if not self.connect.exists(key):
+            # Redis: 两个集合取交集，生成一个新集合，取交集的一个集合是 zset 类型
+            self.connect.zinterstore(key, [f"group:{group}", order], aggregate="max")
+            self.connect.expire(key, 60)
+        return self.get_articles(page, key)
 
 
-def get_article_id(article_key: str) -> str:
-    return article_key.partition(":")[-1]
+class Chapter02:
 
+    def __init__(self, connect: Redis):
+        self.connect = connect
 
-def article_vote(conn: Redis, user: str, article: str):
-    cutoff = time.time() - ONE_WEEK_IN_SECONDS
-    # 规定一篇文章发布期满一周之后，用户将不能再对它进行投票
-    if conn.zscore(f"{pairs.score_pair_key}", article) < cutoff:
-        return
-
-    article_id = get_article_id(article)
-    # Redis: 向数据库中添加 <voted:{article_id}, {user}> 键值对
-    if conn.sadd(f"voted:{article_id}", user):
-        # Redis: 增加 键为 score: 的 zset 类型中的 {article} 元素的分数值
-        conn.zincrby(f"{pairs.score_pair_key}", article, VOTE_SCORE)
-        # Redis: 增加 键为 {article} 的 hash 类型中的以 votes 为键的键值对的值
-        conn.hincrby(article, "votes", 1)
-
-
-def post_article(conn: Redis, user: str, title: str, link: str) -> str:
-    # Redis: 如果以 article: 为键的键值对不存在，则创建并初始化为 1。如果存在则加一。
-    #   conn.incr 在 python 中的返回值是 int 类型
-    article_id = str(conn.incr("article:"))
-
-    voted_key = f"voted:{article_id}"
-    conn.sadd(voted_key, user)
-    # 一周后删除投票用户的记录，因为一周后会冻结投票，显然不必留存了
-    conn.expire(voted_key, ONE_WEEK_IN_SECONDS)
-
-    now_time_stamp = time.time()
-    article_key = f"article:{article_id}"
-    # Redis: 此处是 python 封装的接口，设置 <{article_key}, hash> 键值对
-    conn.hmset(article_key, {
-        "title": title,
-        "link": link,
-        "poster": user,
-        "time": now_time_stamp,
-        "votes": 1  # 默认票数为 1
-    })
-
-    conn.zadd(f"{pairs.score_pair_key}", article_key, now_time_stamp + VOTE_SCORE)
-    conn.zadd(f"{pairs.time_pair_key}", article_key, now_time_stamp)
-
-    return article_id
-
-
-def get_articles(conn: Redis, page, order=f"{pairs.score_pair_key}"):
-    """ 分页返回功能 """
-    start = (page - 1) * ARTICLES_PER_PAGE
-    end = start + ARTICLES_PER_PAGE - 1
-
-    # Redis: 以 {order} 为键的键值对的值（zset 类型）的反向切片 [start, end] 
-    ids = conn.zrevrange(order, start, end)
-    articles = []
-    for article_id in ids:
-        article_data = conn.hgetall(article_id)
-        # 添加字段
-        article_data["id"] = article_id
-        articles.append(article_data)
-    return articles
-
-
-def add_remove_groups(conn: Redis, article_id, to_add=(), to_remove=()):
-    """ 将文章添加到它所在的群组 或 从群组里面移除文章 """
-    article_key = f"article:{article_id}"
-    for e in to_add:
-        conn.sadd(f"group:{e}", article_key)
-    for e in to_remove:
-        conn.srem(f"group:{e}", article_key)
-
-
-def get_group_articles(conn: Redis, group, page, order=f"{pairs.score_pair_key}"):
-    key = order + group
-    if not conn.exists(key):
-        # Redis: 两个集合取交集，生成一个新集合，取交集的一个集合是 zset 类型
-        conn.zinterstore(key, [f"group:{group}", order], aggregate="max")
-        conn.expire(key, 60)
-    return get_articles(conn, page, key)
+    def update_token(self, token, user, viewed_item=None):
+        timestamp = time.time()
+        self.connect.hset("login:", token, user)  # <token,user>
+        self.connect.zadd("recent:", {token: timestamp})
+        if viewed_item:
+            self.connect.zadd(f"viewed:{token}", {viewed_item: timestamp})
+            # 最近浏览过的 25 个商品
+            self.connect.zremrangebyrank(f"viewed:{token}", 0, -26)
 
 
 if __name__ == '__main__':
-    # conn = Redis(host="127.0.0.1", port=6379, db=0)
-    # print(type(conn.incr("article:")))
-    # print(conn.sadd("voted:1", 1))
-    # print(conn.sadd("voted:1", 1))
-    # print(type(conn.sadd("voted:1", 1)))
-    # conn.zinterstore()
-    pass
+    c01 = Chapter01(Redis(host="127.0.0.1", port=6379, db=0))
+    c01.post_article(**dict(user="张世豪", title="test4", link="https://test4"))
+    c01.post_article(**dict(user="张世豪", title="test5", link="https://test5"))
